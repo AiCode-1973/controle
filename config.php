388d2fd7ed0e8d429461fb917b4a3f8c7d527d86
AiCode-1973 <admin@aicode.dev.br>
2026-08-05 21:53:00 -0300

    remove BASE_PATH definition from config

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('APP_NAME', 'Controle de Projetos');
-define('BASE_PATH', '/controle');
+define('BASE_PATH', '');
 define('UPLOAD_DIR', __DIR__ . '/arquivos/uploads/');
 define('MAX_UPLOAD_SIZE', 10 * 1024 * 1024); // 10 MB
 
